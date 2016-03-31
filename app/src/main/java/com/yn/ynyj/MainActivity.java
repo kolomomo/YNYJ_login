8a5976addcfea6e9a734ff3d82b21599644adf34
iwbo <1871756080@qq.com>
2016-03-31 10:56:02 +0800

    wb330

---
+++ app/src/main/java/com/yn/ynyj/MainActivity.java
@@ -0,0 +1,20 @@
+package com.yn.ynyj;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if ((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0)
+        {
+            finish();
+            return;
+        }
+        setContentView(R.layout.login);
+    }
+}
